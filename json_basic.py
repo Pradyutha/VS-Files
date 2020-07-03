bd4a92adad9f02838d9342e321542b2bef11c298
Pradyutha <57126959+Pradyutha@users.noreply.github.com>
2020-07-03 18:24:38 +0530

    read and write -- json

--- json_basic.py
+++ json_basic.py
@@ -8,7 +8,7 @@ with open("data.txt", "w") as outfile:
     json.dump(dictionary, outfile) 
 
 
-with open('sample.json', 'r') as openfile: 
+with open("sample.json", "r") as openfile: 
   
     # Reading from json file 
     json_object = json.load(openfile) 
